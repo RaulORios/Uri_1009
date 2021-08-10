bf4774bac1b082b8a2fac98f30f7e705b7c11af4
RaulORios <raul.orios89@outlook.com>
2021-08-10 11:28:00 -0300

    Arquivos atualizados

---
+++ Uri_1009/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = Console.ReadLine();
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            int comissao = 15;
+            double salario_final = salario + (((double)comissao / 100) * vendas);
+
+            Console.WriteLine($"TOTAL = R$ {salario_final.ToString("F2", CultureInfo.InvariantCulture)}");
+                        
+        }
+    }
+}
